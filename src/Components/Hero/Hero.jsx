041f9a259f5d8a49d5ae945f73273be8fac2a991
Sadman Saquib <sadmansaquib12@gmail.com>
2024-07-07 06:49:21 +0530

    update2: mobile view

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -16,8 +16,8 @@ const Hero = () => {
     className='flex flex-col items-center gap-[35px]'>
       <img src={profile} alt='Image' className='rounded-full w-[200px] mt-10 xl:mt-70px]'></img>
       <h1 className='hidden md:flex text-5xl md:text-6xl font-bold text-center sm:h-10'>I'm <span>Sadman Saquib</span></h1>
-      <h1 className='text-5xl h-8 font-semibold md:hidden'>I'm <span>Sadman</span></h1>
-      <p className=' px-12 md:text-xl h-12 text-center md:h-12  text-white/60'>A Programmer, Graphic Designer & Video Editor</p>
+      <h1 className='text-5xl font-bold text-center sm:h-10 md:hidden'>I'm <span>Sadman</span></h1>
+      <p className=' px-10 md:text-xl text-center h-12  text-white/60'>A Programmer, Graphic Designer & Video Editor</p>
       <div className="hero-action flex flex-col gap-3 items-center text-[16px] font-semibold xl:gap-[25px] xl:flex-row mb-[50px]">
         <div className="hero-connect"><img src={send} alt="" />Say Hello</div>
         <div className="hero-resume"><img src={resume} alt="" />My Resume</div>
