a132b970c1e2333bae9f4d65cf19e3116a3c0899
Sadman Saquib <sadmansaquib12@gmail.com>
2024-07-08 02:53:55 +0530

    fix-issue

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { defineConfig } from "vite"
 
 export default defineConfig({
   plugins: [react()],
-  base: '/portfolio-sadman/',
+  base: 'portfolio-sadman',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
